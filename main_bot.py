45ee1f2cf7e99304a8fc3e10340560d2564b8ece
RucioleWind <159928785+RucioleWind@users.noreply.github.com>
2025-04-09 23:26:37 +0900

    アクティビティ表示

--- main_bot.py
+++ main_bot.py
@@ -17,7 +17,13 @@ async def on_ready():
         print(f"スラッシュコマンドを同期しました: {len(synced)}個")
     except Exception as e:
         print(e)
-    await on_ready.response.send_message("団員情報を表示中！")
+   # await on_ready.response.send_message("団員情報を表示中！")　←これはターミナルに表示させるときの処理？
+
+@bot.event
+async def on_ready():
+    activity = discord.Game(name="団員情報に接続しています")
+    await bot.change_presence(status=discord.Status.online, activity=activity)
+    print(f"準備完了！：{bot.user}")
 
 async def main():
     async with bot:
